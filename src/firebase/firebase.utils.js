a643702e845ab967ee440c815833ed98add1f1b0
arielyiyitou <my442@drexel.edu>
2021-06-18 00:51:57 -0400

    Add home page, and enable sign in and sign up with Firebase

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,50 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyBW3z0fTtY6bJyByvGcUE2mYz0C7rS93Qs",
+    authDomain: "mock-ecommerce-7728a.firebaseapp.com",
+    projectId: "mock-ecommerce-7728a",
+    storageBucket: "mock-ecommerce-7728a.appspot.com",
+    messagingSenderId: "632219037036",
+    appId: "1:632219037036:web:b99df7496dcb1c417dd749",
+    measurementId: "G-M7TF1TNVEY"
+};
+
+
+firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) { return; }
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
